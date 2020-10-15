c3633e94d0a3f6865550d149abc48ca1773db352
Lyapk <Gips009999@mail.ru>
2020-10-15 21:37:20 +0300

    Lesson3

--- app/src/main/java/com/dlyapkov/weather/CityActivity.java
+++ app/src/main/java/com/dlyapkov/weather/CityActivity.java
@@ -1,6 +1,5 @@
 package com.dlyapkov.weather;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
--- app/src/main/java/com/dlyapkov/weather/MainActivity.java
+++ app/src/main/java/com/dlyapkov/weather/MainActivity.java
@@ -3,13 +3,8 @@ package com.dlyapkov.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.view.KeyEvent;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
