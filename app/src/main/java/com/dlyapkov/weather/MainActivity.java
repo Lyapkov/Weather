be110b6c5631fc7cdd5d98b8b47ed8d2e2487436
Lyapk <Gips009999@mail.ru>
2020-10-25 19:22:26 +0300

    Lesson6

--- app/src/main/java/com/dlyapkov/weather/MainActivity.java
+++ app/src/main/java/com/dlyapkov/weather/MainActivity.java
@@ -16,5 +16,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initDataSource();
+    }
+
+    private void initDataSource() {
+        NodeDataSource sourceData = new NodeSourceBuilder().setResources(getResources()).build();
+
+        final NodeChangebleSource nodeChangeSource = new NodChangableSource(sourceData);
+        final NodeAdapter adapter = initRecyclerView(nodeChangeSource);
+
+        adapter.SetOnItemClickListener(new NodeAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+
+            }
+        });
+
+        nodeChangeSource.add();
+        nodeChangeSource.add();
+        nodeChangeSource.add();
+        nodeChangeSource.add();
+        nodeChangeSource.add();
+        adapter.notifyItemInserted(nodeChangeSource.size());
+    }
+
+    private NodeAdapter initRecyclerView(NodeChangebleSource nodeChangebleSource) {
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+
+
+
+        recyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(this, LinearLayoutManager.VERTICAL);
+
+        recyclerView.addItemDecoration(itemDecoration);
+
+        NodeAdapter adapter = new NodeAdapter(nodeChangebleSource);
+
+
+        recyclerView.setAdapter(adapter);
+
+        return adapter;
     }
 }
